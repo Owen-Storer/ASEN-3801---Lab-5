c53c4980d7951c87fb61c89341f8da3b51794782
MatMend <mame8179@colorado.edu>
2025-11-18 10:18:28 -0700

    Add files via upload

---
+++ TransformFromInertialToBody.m
@@ -0,0 +1,17 @@
+function [wind_body] = TransformFromInertialToBody(wind_inertial, aircraft_state)
+
+phi = aircraft_state(1);
+theta = aircraft_state(2);
+psi = aircraft_state(3);
+
+% DCM matrix 
+R_b2i = [ cos(theta)*cos(psi),  sin(phi)*sin(theta)*cos(psi) - cos(phi)*sin(psi),  cos(phi)*sin(theta)*cos(psi) + sin(phi)*sin(psi);
+          cos(theta)*sin(psi),  sin(phi)*sin(theta)*sin(psi) + cos(phi)*cos(psi),  cos(phi)*sin(theta)*sin(psi) - sin(phi)*cos(psi);
+          -sin(theta),          sin(phi)*cos(theta),                               cos(phi)*cos(theta) ]; 
+
+% transpose 
+R_i2b = R_b2i';
+
+wind_body = wind_inertial .* R_i2b; 
+
+end 
